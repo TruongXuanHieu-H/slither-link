512450af2c5601d6d4c70aa3c0ddcc714721049e
Trương Xuân Hiếu <truongxuanhieu11@vnu.edu.vn>
2024-04-17 15:52:02 +0700

    fix

--- exporter.py
+++ exporter.py
@@ -5,8 +5,6 @@ import pandas as pd
 from pysat.solvers import Minisat22
 
 import SlitherLinkAddAllLoop
-import SlitherLinkOrigin
-import SlitherLinkPreloading
 
 test_folder = glob.glob("puzzle/*.txt", recursive=True)
 
@@ -14,16 +12,6 @@ all_loop_base_condition = []
 all_loop_total_condition = []
 all_loop_loop_count = []
 all_loop_time_elapsed = []
-
-origin_base_condition = []
-origin_total_condition = []
-origin_loop_count = []
-origin_time_elapsed = []
-
-my_base_condition = []
-my_total_condition = []
-my_loop_count = []
-my_time_elapsed = []
 
 
 def process(solver, file, base_condition, total_condition, loop_count, time_elapsed):
@@ -45,42 +33,24 @@ for file in test_folder:
         SlitherLinkAddAllLoop.SlitherLinkAddAllLoop(Minisat22), file, all_loop_base_condition,
         all_loop_total_condition,
         all_loop_loop_count, all_loop_time_elapsed)
-    print("add my")
-    my_base_condition, my_total_condition, my_loop_count, my_time_elapsed = process(
-        SlitherLinkPreloading.SlitherLinkPreloading(Minisat22), file, my_base_condition, my_total_condition,
-        my_loop_count, my_time_elapsed)
-    print("add origin")
-    origin_base_condition, origin_total_condition, origin_loop_count, origin_time_elapsed = process(
-        SlitherLinkOrigin.SlitherLinkOrigin(Minisat22), file, origin_base_condition,
-        origin_total_condition,
-        origin_loop_count, origin_time_elapsed)
 
 all_loop_base_condition.append(sum(all_loop_base_condition))
 all_loop_total_condition.append(sum(all_loop_total_condition))
 all_loop_loop_count.append(sum(all_loop_loop_count))
 all_loop_time_elapsed.append(sum(all_loop_time_elapsed))
 
-my_base_condition.append(sum(my_base_condition))
-my_total_condition.append(sum(my_total_condition))
-my_loop_count.append(sum(my_loop_count))
-my_time_elapsed.append(sum(my_time_elapsed))
-
-origin_base_condition.append(sum(origin_base_condition))
-origin_total_condition.append(sum(origin_total_condition))
-origin_loop_count.append(sum(origin_loop_count))
-origin_time_elapsed.append(sum(origin_time_elapsed))
-
 test_folder.append('total')
-#print(len(test_folder), len(add_all_loop_base_condition))
 
 data = pd.DataFrame({"file_test": test_folder,
-                     "add_all_loop_base_condition": all_loop_base_condition, "origin_base_condition": origin_base_condition, "my_base_condition": my_base_condition,
-                     "add_all_loop_total_condition": all_loop_total_condition, "origin_total_condition": origin_total_condition, "my_total_condition": my_total_condition,
-                     "add_all_loop_loop_count": all_loop_loop_count, "origin_loop_count": origin_loop_count, "my_loop_count": my_loop_count,
-                     "add_all_loop_time_elapsed": all_loop_time_elapsed, "origin_time_elapsed": origin_time_elapsed, "my_time_elapsed": my_time_elapsed})
+                     "add_all_loop_base_condition": all_loop_base_condition,
+                     "add_all_loop_total_condition": all_loop_total_condition,
+                     "add_all_loop_loop_count": all_loop_loop_count,
+                     "add_all_loop_time_elapsed": all_loop_time_elapsed})
+
 
 def df_style(x):
     return 'font-weight: bold'
+
 
 last_row = pd.IndexSlice[data.index[-1], :]
 writer = pd.ExcelWriter('output.xlsx')
@@ -90,6 +60,5 @@ for column in data:
     column_length = max(data[column].astype(str).map(len).max(), len(column))
     col_idx = data.columns.get_loc(column)
     writer.sheets['sheet1'].set_column(col_idx, col_idx, column_length)
-
 
 writer.close()
